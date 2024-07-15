96e61412aac2b2eac4f2e709ea57d10ea8e2876a
Oauthus42 <var.verdandi@gmail.com>
2024-07-15 11:37:45 +0300

    ui. чат, отправка сообщения, инпуты, emoji picker

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import './chat.css';
+import EmojiPicker from 'emoji-picker-react';
 
 const Chat = () => {
+const [open, setOpen] = useState(false);
+const [text, setText] = useState("");
+console.log(text)
+
+
+const handleEmoji = (e) => {
+    setText ((prev) => prev + e.emoji)
+    setOpen(false)
+}
+
     return (
         <div className="chat">
             <div className="top">
@@ -20,7 +32,27 @@ const Chat = () => {
             </div>
 
             <div className="center">center</div>
-            <div className="bottom">bottom</div>
+
+            <div className="bottom">
+                <div className="icons">
+                    <img src='./img.png'></img>
+                    <img src='./camera.png'></img>
+                    <img src='./mic.png'></img>
+                </div>
+                <input type='text' placeholder='Введите сообщение'
+                value={text}
+                onChange={e => setText(e.target.value)}>
+                </input>
+                <div className="emoji">
+                    <img src='./emoji.png'
+                    onClick={() => setOpen((prev) => !prev)}>
+                    </img>
+                    <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+                    </div>
+                </div>
+                <button className='sendButton'>Отправить</button>
+            </div>
         </div>
     )
 }
