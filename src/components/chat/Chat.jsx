d01e7f526fe9c751f13eb7780cb58d92fbf36186
Oauthus42 <var.verdandi@gmail.com>
2024-07-15 12:04:04 +0300

    ui. сообщения внутри чата

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -31,7 +31,33 @@ const handleEmoji = (e) => {
                 </div>
             </div>
 
-            <div className="center">center</div>
+            <div className="center">
+
+                <div className="message">
+                    <img src='./avatar6.png'></img>
+                    <div className="texts">
+                        <p>Исходящее сообщение</p>
+                        <span>15 минут назад</span>
+                    </div>
+                </div>
+
+                <div className="message own">
+                    <div className="texts">
+                        <p>Входящее сообщение</p>
+                        <span>10 минут назад</span>
+                    </div>
+                </div>
+
+                <div className="message">
+                    <img src='./avatar6.png'></img>
+                    <div className="texts">
+                        <img src='https://colodu.club/uploads/posts/2022-11/1667450724_14-colodu-club-p-gerbarii-v-interere-vkontakte-14.jpg'></img>
+                        <p>Исходящее сообщение</p>
+                        <span>4 минуты назад</span>
+                    </div>
+                </div>
+
+            </div>
 
             <div className="bottom">
                 <div className="icons">
