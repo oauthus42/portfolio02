df86bbffd432a00fd545541de866a9ce2171ea12
Oauthus42 <var.verdandi@gmail.com>
2024-07-16 14:07:52 +0300

    регистрация и авторизация

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,6 +1,10 @@
-import { useState } from 'react';
 import './login.css'
+import { useState } from 'react';
 import { toast } from 'react-toastify';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../../lib/firebase";
+import { doc, setDoc } from '@firebase/firestore';
+import upload from '../../lib/upload';
 
 const Login = () => {
 //состояние для отображения загруженной фотографии
@@ -9,19 +13,63 @@ const [avatar, setAvatar] = useState({
     url: ''
 })
 
+const [loading, setLoading] = useState(false)
+
+
 const handleAvatar = (e) => {
     if(e.target.files[0]) {
-    setAvatar ({
-        file: e.target.files[0],
-        url: URL.createObjectURL(e.target.files[0])
-    })
-}
+        setAvatar ({
+            file: e.target.files[0],
+            url: URL.createObjectURL(e.target.files[0])
+         })
+    }
 }
 
-const handleLogin = (e) => {
+const handleRegister = async (e) => {
     e.preventDefault()
-}
+    setLoading(true)
+    const formData = new FormData(e.target);
+    const {username, email, password} = Object.fromEntries(formData);
 
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password)
+        const imgURL = await upload(avatar.file);
+
+        await setDoc(doc(db, "username", res.user.uid), {
+            username,
+            email,
+            avatar: imgURL,
+            id: res.user.uid,
+            blocked: []
+        })
+
+        await setDoc(doc(db, "userchats", res.user.uid), {
+            chats: []
+        });
+
+        toast.success("Аккаунт успешно создан!");
+    } catch (error) { 
+        console.log(error);
+        toast.error(error.message);
+    } finally {
+        setLoading(false);
+    }
+};
+
+const handleLogin = async (e) => {
+    setLoading(true);
+    const formData = new FormData(e.target);
+    const {email, password} = Object.fromEntries(formData);
+
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.log(error.message);
+        toast.error(error.message);
+    } finally {
+        setLoading(false);
+    }
+}
 
     return (
         <div className='login'>
@@ -30,7 +78,7 @@ const handleLogin = (e) => {
                 <form onSubmit={handleLogin}>
                     <input type='text' placeholder='e-mail' name='email'></input>
                     <input type='password' placeholder='пароль' name='password'></input>
-                    <button>Войти</button>
+                    <button disabled={loading}>{loading ? 'Загрузка...' : 'Войти'}</button>
                 </form>
             </div>
 
@@ -38,7 +86,7 @@ const handleLogin = (e) => {
 
             <div className="item">
             <h2>Создать аккаунт</h2>
-                <form>
+                <form onSubmit={handleRegister}>
                     <img src={avatar.url || './avatar.png'}></img>
                     <label htmlFor='file'>Загрузить фотографию</label>
                     <input type='file' id='file' 
@@ -48,7 +96,7 @@ const handleLogin = (e) => {
                     <input type='text' placeholder='логин' name='username'></input>
                     <input type='text' placeholder='e-mail' name='email'></input>
                     <input type='password' placeholder='пароль' name='password'></input>
-                    <button>Создать</button>
+                    <button disabled={loading}>{loading ? 'Загрузка...' : 'Создать'}</button>
                 </form>
             </div>
         </div>
