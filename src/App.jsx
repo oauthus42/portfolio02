7209af6c002bd570879c8531252ee17d1c0bf1b7
Oauthus42 <var.verdandi@gmail.com>
2024-07-17 17:43:13 +0300

    реагирование на состояние аутентификации (zustand)

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,45 @@
 import { useEffect } from "react";
-import Chat from "./components/chat/Chat"
-import Detail from "./components/detail/Detail"
-import List from "./components/list/List"
-import Login from "./components/login/Login";
-import Notification from "./components/notification/Notification";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
-
+import { useUserStore } from "./lib/userStore";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
 
 const App = () => {
-  const user = false;
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+  //потом пофиксить, и в рендере тоже:
+  const user = true;
 
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
-      console.log(user)
+      fetchUserInfo(user?.uid);
     });
+
     return () => {
       unSub();
-    }
-  }, [])
+    };
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div className="loading">Загрузка...</div>;
+  console.log(currentUser)
 
   return (
-    <div className='container'>
-    {user ? (
-      <>
-      <List />
-      <Chat />
-      <Detail />
-    </>
-    ) : (
-      <Login />
-    )}
-    <Notification />
+    <div className="container">
+      {user ? (
+        <>
+          <List />
+          <Chat />
+          <Detail />
+        </>
+      ) : (
+        <Login />
+      )}
+      <Notification />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
